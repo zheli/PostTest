792a289b68d9d2f372f1f3555c08ed83ab60f5c4
Zhe Li <zheli.se.gb@gmail.com>
2011-10-29 19:11:44 +0200

    Initial Commit

---
+++ HelloWorldAgain/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HelloWorldAgain
+//
+//  Created by Li Zhe on 10/29/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HelloWorldAgain/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HelloWorldAgain
+//
+//  Created by Li Zhe on 10/29/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
